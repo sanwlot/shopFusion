9a23ff09ed58b42aa1f4e384205b95b231d73a33
sunnySavlot <sunny.savlot@gmail.com>
2024-03-24 10:23:46 +0530

    refactored the code

--- src/StateProvider.jsx
+++ src/StateProvider.jsx
@@ -1,14 +1,13 @@
 import { createContext, useContext, useReducer } from "react";
-
-// prepares the data layer
 export const StateContext = createContext();
 
-// wraps our app and provide the data layer to all our components
-export const StateProvider = ({ reducer, initialState, children }) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>
-);
+export function StateProvider({ reducer, initialState, children }) {
+  return (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </StateContext.Provider>
+  );
+}
 
 // pull information from the data layer
 export const useStateValue = () => useContext(StateContext);
