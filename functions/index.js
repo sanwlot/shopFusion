8135533176ffe266744ca113d4d3290bbe71403c
sunnySavlot <sunny.savlot@gmail.com>
2024-10-13 18:43:09 +0530

    sk env var added

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,34 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const express = require("express");
-const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51ONpPlSF7FnDkPDAqpnQKBGq0B2U80dM6w0cZ0fAizfAipxVhX0Af49Vefrd3xtegWj14cjMDhU8G6cAtzzr1yzG00PDsGzXqy"
-);
+const { onRequest } = require("firebase-functions/v2/https")
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+const stripe = require("stripe")(process.env.STRIPE_SK)
 
 // App config
-const app = express();
+const app = express()
 
 // middleware
-app.use(cors({ origin: true }));
-app.use(express.json());
+app.use(cors({ origin: true }))
+app.use(express.json())
 
 // API routes
-app.get("/", (req, res) => res.status(200).send("Server is running!"));
+app.get("/", (req, res) => res.status(200).send("Server is running!"))
 
 app.post("/payments/create", async (req, res) => {
-  const total = req.query.total;
+  const total = req.query.total
 
-  console.log("Payment request received!!! ", total);
+  console.log("Payment request received!!! ", total)
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: parseFloat(total), // subunits of the currency
     currency: "inr",
-  });
+  })
 
   // OK - created
   res.status(201).send({
     clientSecret: paymentIntent.client_secret,
-  });
-});
+  })
+})
 
 // Listen command
-exports.api = onRequest(app);
+exports.api = onRequest(app)
